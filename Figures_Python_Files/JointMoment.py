8b3d95923f2b3438f53e8f412b812f236ac51205
Bonab <alik@sabanciuniv.local>
2020-02-28 16:38:22 +0300

    Utils.py has been created to develop functions that is needed for the figures.

--- Figures_Python_Files/JointMoment.py
+++ Figures_Python_Files/JointMoment.py
@@ -16,6 +16,7 @@ from numpy import nanmean, nanstd
 from perimysium import postprocessing as pp
 from perimysium import dataman
 import pathlib
+import Utils as utils
 #####################################################################################
 #####################################################################################
 # Reading CSV files into a dictionary
@@ -24,4 +25,12 @@ files = enumerate(glob.iglob(directory), 1)
 dataset = {pathlib.PurePath(f[1]).stem: np.loadtxt(f[1], delimiter=',') for f in files}
 #####################################################################################
 # Processing Data
+subjects = ['05','07','09','10','11','12','14']
+trials_num = ['01','02','03']
+gl_noload = {'noload_subject{}_trial{}'.format(i,j): utils.construct_gl_mass_side(subjectno=i,trialno=j,loadcond='noload') for i in subjects for j in trials_num}
+gl_loaded = {'loaded_subject{}_trial{}'.format(i,j): utils.construct_gl_mass_side(subjectno=i,trialno=j,loadcond='loaded') for i in subjects for j in trials_num}
+noload_mean_toe_off,_,loaded_mean_toe_off,_ = utils.toe_off_avg_std(gl_noload,gl_loaded)
+
+        
+
 
--- Figures_Python_Files/Utils.py
+++ Figures_Python_Files/Utils.py
@@ -18,7 +18,21 @@ from perimysium import dataman
 import pathlib
 #######################################################################
 #######################################################################
-def construct_gl_mass_trial(subjectno,trialno,loadcond):
+def toeoff_pgc(gl, side):
+    if side == 'right':
+        toeoff = gl.right_toeoff
+        strike = gl.right_strike
+    elif side == 'left':
+        toeoff = gl.left_toeoff
+        strike = gl.left_strike
+    cycle_duration = (gl.cycle_end - gl.cycle_start)
+    while toeoff < strike:
+        toeoff += cycle_duration
+    while toeoff > strike + cycle_duration:
+        toeoff -= cycle_duration
+    return pp.percent_duration_single(toeoff,strike,strike + cycle_duration)
+
+def construct_gl_mass_side(subjectno,trialno,loadcond):
     """This function has been designed to construct gl from the dataset. It also returns subject mass
        and trial number to be used on other functions"""
     import Subjects_Dataset as sd
@@ -29,16 +43,36 @@ def construct_gl_mass_trial(subjectno,trialno,loadcond):
     else:
         raise Exception("load condition is wrong.")
     mass = data["mass"]
-    trial_num = data["trial"]
-    gl = dataman.GaitLandmarks( primary_leg = data["primary_legs"],
-                                cycle_start = data["subjects_cycle_start_time"],
-                                cycle_end   = data["subjects_cycle_end_time"],
-                                left_strike = data["footstrike_left_leg"],
-                                left_toeoff = data["toeoff_time_left_leg"],
-                                right_strike= data["footstrike_right_leg"],
-                                right_toeoff= data["toeoff_time_right_leg"])
-    return gl, mass, trial_num 
+    side = data["primary_legs"]
+    gl = dataman.GaitLandmarks( primary_leg = data['primary_legs'],
+                                cycle_start = data['subjects_cycle_start_time'],
+                                cycle_end   = data['subjects_cycle_end_time'],
+                                left_strike = data['footstrike_left_leg'],
+                                left_toeoff = data['toeoff_time_left_leg'],
+                                right_strike= data['footstrike_right_leg'],
+                                right_toeoff= data['toeoff_time_right_leg'])
+    return gl,mass,side
+
 def mean_std_over_subjects(data):
     mean = np.nanmean(data,axis=1)
     std = np.nanstd(data,axis=1)
 
+def toe_off_avg_std(gl_noload,gl_loaded):
+    '''This function returns the mean toe off percentage for loaded and noloaded subjects
+        parameters:
+            gl_noload: a dictionary of noload subjects gait landmark
+            gl_loaded: a dictionary of loaded subjects gait landmark
+        output:
+            np.mean(noload_toe_off),np.std(noload_toe_off),np.mean(loaded_toe_off),np.std(loaded_toe_off)
+        '''
+    noload_toe_off= np.zeros(21)
+    loaded_toe_off= np.zeros(21)
+    c0 = 0
+    c1 = 0
+    for key in gl_noload.keys():
+        noload_toe_off[c0] =toeoff_pgc(gl=gl_noload[key][0],side= gl_noload[key][2])
+        c0+=1
+    for key in gl_loaded.keys():
+        loaded_toe_off[c1] =toeoff_pgc(gl=gl_loaded[key][0],side= gl_loaded[key][2])
+        c1+=1
+    return np.mean(noload_toe_off),np.std(noload_toe_off),np.mean(loaded_toe_off),np.std(loaded_toe_off)
