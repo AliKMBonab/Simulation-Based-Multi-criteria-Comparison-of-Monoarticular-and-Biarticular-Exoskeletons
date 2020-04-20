685938c75ec476ada1c094b9005f97e68cd62401
Bonab <alik@sabanciuniv.local>
2020-04-20 18:17:56 +0300

    New specific weights data prepration and plotting

--- Figures/Specific_Weights/LoadedMono01_LoadedBi11/SpecificWeight_EnergyPlots_Figures.py
+++ Figures/Specific_Weights/LoadedMono01_LoadedBi11/SpecificWeight_EnergyPlots_Figures.py
@@ -43,22 +43,22 @@ gl_loaded = {'loaded_subject{}_trial{}'.format(i,j): utils.construct_gl_mass_sid
 # toe-off
 noload_mean_toe_off,_,loaded_mean_toe_off,_ = utils.toe_off_avg_std(gl_noload,gl_loaded)
 # Energy
-noload_bi_metabolics = utils.reduction_calc(unassisted_energy_dataset['noload_metabolics_energy'],assisted_energy_dataset['biarticular_hip50knee40_noload_metabolics_energy'])
-noload_mono_metabolics = utils.reduction_calc(unassisted_energy_dataset['noload_metabolics_energy'],assisted_energy_dataset['monoarticular_hip50knee50_noload_metabolics_energy'])
+loaded_bi_metabolics = utils.reduction_calc(unassisted_energy_dataset['loaded_metabolics_energy'],assisted_energy_dataset['biarticular_hip50knee70_load_metabolics_energy'])
+loaded_mono_metabolics = utils.reduction_calc(unassisted_energy_dataset['loaded_metabolics_energy'],assisted_energy_dataset['monoarticular_hip70knee70_load_metabolics_energy'])
 #  Mean/std energy percent
-mean_noload_bi_metabolics, std_noload_bi_metabolics = utils.mean_std_over_subjects(noload_bi_metabolics,ax=0)
-mean_noload_mono_metabolics, std_noload_mono_metabolics = utils.mean_std_over_subjects(noload_mono_metabolics,ax=0)
+mean_loaded_bi_metabolics, std_loaded_bi_metabolics = utils.mean_std_over_subjects(loaded_bi_metabolics,ax=0)
+mean_loaded_mono_metabolics, std_loaded_mono_metabolics = utils.mean_std_over_subjects(loaded_mono_metabolics,ax=0)
 # Mean/std energy
-mean_noload_unassist_energy,std_noload_unassist_energy = utils.mean_std_over_subjects(unassisted_energy_dataset['noload_metabolics_energy'],ax=0)
-mean_noload_bi_energy,std_noload_bi_energy = utils.mean_std_over_subjects(assisted_energy_dataset['biarticular_hip50knee40_noload_metabolics_energy'],ax=0)
-mean_noload_mono_energy,std_noload_mono_energy = utils.mean_std_over_subjects(assisted_energy_dataset['monoarticular_hip50knee50_noload_metabolics_energy'],ax=0)
+mean_loaded_unassist_energy,std_loaded_unassist_energy = utils.mean_std_over_subjects(unassisted_energy_dataset['loaded_metabolics_energy'],ax=0)
+mean_loaded_bi_energy,std_loaded_bi_energy = utils.mean_std_over_subjects(assisted_energy_dataset['biarticular_hip50knee70_load_metabolics_energy'],ax=0)
+mean_loaded_mono_energy,std_loaded_mono_energy = utils.mean_std_over_subjects(assisted_energy_dataset['monoarticular_hip70knee70_load_metabolics_energy'],ax=0)
 
 #####################################################################################
 # Plots
 # Biarticular VS Monoarticular
-names = ['unassist, noload','mono, noload','bi, noload']
-data = [mean_noload_unassist_energy,mean_noload_mono_energy,mean_noload_bi_energy]
-err = [std_noload_unassist_energy,std_noload_mono_energy,std_noload_bi_energy]
+names = ['unassist, loaded','mono, loaded','bi, loaded']
+data = [mean_loaded_unassist_energy,mean_loaded_mono_energy,mean_loaded_bi_energy]
+err = [std_loaded_unassist_energy,std_loaded_mono_energy,std_loaded_bi_energy]
 fig, ax = plt.subplots(figsize=(6.4, 4.8))
 barplot = ax.bar(names, data, yerr=err, align='center',color=mycolors['manatee grey'],width=0.45, ecolor='black', capsize=10)
 barplot[1].set_color(mycolors['olympic blue'])
@@ -67,14 +67,14 @@ ax.set_ylabel('Metabolic Rate (W/Kg)')
 ax.set_xticks(names)
 ax.set_xticklabels(names)
 utils.no_top_right(ax)
-fig.savefig('./Figures/Specific_Weights/NoloadMono13_NoloadBi14/Metabolic_Rate_BarPlot.pdf',orientation='landscape',bbox_inches='tight')
+fig.savefig('./Figures/Specific_Weights/LoadedMono01_LoadedBi11/Metabolic_Rate_BarPlot.pdf',orientation='landscape',bbox_inches='tight')
 
 #******************************************************************
 # Metabolic Bar Plot
-# Noload Mono Vs Bi
-names = ['bi, noload','mono, noload']
-data = [mean_noload_bi_metabolics,mean_noload_mono_metabolics]
-err = [std_noload_bi_metabolics,std_noload_mono_metabolics]
+# loaded Mono Vs Bi
+names = ['bi, loaded','mono, loaded']
+data = [mean_loaded_bi_metabolics,mean_loaded_mono_metabolics]
+err = [std_loaded_bi_metabolics,std_loaded_mono_metabolics]
 fig, ax = plt.subplots(figsize=(6.4, 4.8))
 barplot = ax.bar(names, data, yerr=err, align='center',color=mycolors['manatee grey'],width=0.45, ecolor='black', capsize=10)
 barplot[1].set_color(mycolors['dark purple'])
@@ -85,17 +85,17 @@ ax.set_xticklabels(names)
 utils.no_top_right(ax)
 fig.tight_layout()
 plt.show()
-fig.savefig('./Figures/Specific_Weights/NoloadMono13_NoloadBi14/Metabolic_Percent_ParPlot.pdf',orientation='landscape',bbox_inches='tight')
+fig.savefig('./Figures/Specific_Weights/LoadedMono01_LoadedBi11/Metabolic_Percent_ParPlot.pdf',orientation='landscape',bbox_inches='tight')
 
 
 #******************************************************************
 # Metabolic Box Plot
 # Biarticular
 
-names = ['unassist, noload','mono, noload','bi, noload']
+names = ['unassist, loaded','mono, loaded','bi, loaded']
 x = np.arange(1,len(names)+1,1)
-data = [unassisted_energy_dataset['noload_metabolics_energy'],\
-        assisted_energy_dataset['monoarticular_hip50knee50_noload_metabolics_energy'],assisted_energy_dataset['biarticular_hip50knee40_noload_metabolics_energy'][~np.isnan(assisted_energy_dataset['biarticular_hip50knee40_noload_metabolics_energy'])]]
+data = [unassisted_energy_dataset['loaded_metabolics_energy'],\
+        assisted_energy_dataset['monoarticular_hip70knee70_load_metabolics_energy'],assisted_energy_dataset['biarticular_hip50knee70_load_metabolics_energy'][~np.isnan(assisted_energy_dataset['biarticular_hip50knee70_load_metabolics_energy'])]]
 fig, ax = plt.subplots(figsize=(6.4, 4.8))
 bp = ax.boxplot(data, patch_artist=True)
 utils.beautiful_boxplot(bp)
@@ -105,17 +105,17 @@ ax.set_xticklabels(names)
 utils.no_top_right(ax)
 fig.tight_layout()
 plt.show()
-fig.savefig('./Figures/Specific_Weights/NoloadMono13_NoloadBi14/Metabolic_Rate_BoxPlot.pdf',orientation='landscape',bbox_inches='tight')
+fig.savefig('./Figures/Specific_Weights/LoadedMono01_LoadedBi11/Metabolic_Rate_BoxPlot.pdf',orientation='landscape',bbox_inches='tight')
 
 
 #******************************************************************
 # Actuators Energy Box Plot
-# Biarticular Vs Monoarticular Noload
-names = ['bi hip, noload','bi knee, noload','mono hip, noload','mono knee, noload',]
+# Biarticular Vs Monoarticular loaded
+names = ['bi hip, loaded','bi knee, loaded','mono hip, loaded','mono knee, loaded',]
 x = np.arange(1,len(names)+1,1)
-data = [assisted_energy_dataset['biarticular_hip50knee40_noload_hipactuator_energy'][~np.isnan(assisted_energy_dataset['biarticular_hip50knee40_noload_hipactuator_energy'])],\
-        assisted_energy_dataset['biarticular_hip50knee40_noload_kneeactuator_energy'][~np.isnan(assisted_energy_dataset['biarticular_hip50knee40_noload_kneeactuator_energy'])],\
-        assisted_energy_dataset['monoarticular_hip50knee50_noload_hipactuator_energy'],assisted_energy_dataset['monoarticular_hip50knee50_noload_kneeactuator_energy']]
+data = [assisted_energy_dataset['biarticular_hip50knee70_load_hipactuator_energy'][~np.isnan(assisted_energy_dataset['biarticular_hip50knee70_load_hipactuator_energy'])],\
+        assisted_energy_dataset['biarticular_hip50knee70_load_kneeactuator_energy'][~np.isnan(assisted_energy_dataset['biarticular_hip50knee70_load_kneeactuator_energy'])],\
+        assisted_energy_dataset['monoarticular_hip70knee70_load_hipactuator_energy'],assisted_energy_dataset['monoarticular_hip70knee70_load_kneeactuator_energy']]
 fig, ax = plt.subplots(figsize=(6.4, 4.8))
 bp = ax.boxplot(data, patch_artist=True)
 utils.beautiful_boxplot(bp)
@@ -125,4 +125,4 @@ ax.set_xticklabels(names)
 utils.no_top_right(ax)
 fig.tight_layout()
 plt.show()
-fig.savefig('./Figures/Specific_Weights/NoloadMono13_NoloadBi14/Actuator_Energy_BoxPlot.pdf',orientation='landscape',bbox_inches='tight')
+fig.savefig('./Figures/Specific_Weights/LoadedMono01_LoadedBi11/Actuator_Energy_BoxPlot.pdf',orientation='landscape',bbox_inches='tight')
