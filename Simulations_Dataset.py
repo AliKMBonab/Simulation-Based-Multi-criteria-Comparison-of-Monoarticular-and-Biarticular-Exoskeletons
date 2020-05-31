bc03c3439b5890e20b2379f2fcdb1874c1c2abe8
Personal-Laptop <alikhalilianmotamed@gmail.com>
2020-06-01 01:31:42 +0300

    The data extraction of  reaction force analysis has been developeed. It requires testing and deubugging.

--- Simulations_Dataset.py
+++ Simulations_Dataset.py
@@ -146,8 +146,8 @@ suffixes = ['torque','torque','power','power',\
             'regenerative_energy','regenerative_energy',\
             'power','power','max_power','max_power','avg_positive_power','avg_positive_power',\
             'avg_negative_power','avg_negative_power']
-HWs = {'mono_load':[70,70,70,30],'bi_load':[70,50,50,40,30],'mono_noload':[70,60,50,30],'bi_noload':[70,50,30,30]}
-KWs = {'mono_load':[70,40,30,30],'bi_load':[70,70,60,70,50],'mono_noload':[70,70,30,30],'bi_noload':[70,60,70,50]}
+HWs = {'mono_load':[30,40,50,60,70,70,70,70,70],'bi_load':[30,30,30,30,30,40,40,50,50,50,60,70],'mono_noload':[30,40,50,50,50,60,60,60,70,70],'bi_noload':[30,30,30,30,30,40,40,40,50,50,50,70]}
+KWs = {'mono_load':[30,30,30,30,30,40,50,60,70],'bi_load':[30,40,50,60,70,60,70,50,60,70,70,70],'mono_noload':[30,30,30,40,50,50,60,70,60,70],'bi_noload':[30,40,50,60,70,40,50,60,50,60,70,70]}
 labeling = ['mono','bi']
 #***************************
 print('Section 04:\t extracting Specific Weights of Pareto exoskeletons related files: Actuators Data, Muscles Activation, Muscles Moment, Metabolic Energy.\n')
@@ -163,4 +163,25 @@ if  y.lower() == 'y':
                 out= fcns.specific_weight_data_subjects(configuration=configs[i],HipWeight=HW[j],KneeWeight=KW[j],loadcond=load_type,regenergy=True)
                 for k in range(len(out)):
                     np.savetxt('./Data/Specific_Weights/{}_hip{}knee{}_{}_{}_{}.csv'.format(config_names[i],HW[j],KW[j],load_type,middle[k],suffixes[k]), out[k], fmt='%s', delimiter=',')
-                    
+
+#####################################################################################
+#####################################################################################
+"""Section 05:
+                reaction forces dataset
+"""
+loads = ['noload','loaded']
+configs = [None,'Monoarticular','Biarticular','Monoarticular','Biarticular']
+config_names = ['unassist','monoarticular_ideal','biarticular_ideal','monoarticular_paretofront','biarticular_paretofront']
+cases = ['Unassist','Ideal','Ideal','Paretofront','Paretofront']
+#***************************
+print('Section 04:\t extracting reaction forces')
+#***************************
+y = input('reaction forces data extraction? (y,n):  ')
+if  y.lower() == 'y':
+    print('reaction forces biarticular/monoarticular loaded/noload files are getting extracted the file.\n')
+    for load_type in loads:
+        for i in range(len(configs)):
+            out = fcns.extract_reaction_forces(loadcondition=load_type,case=cases[i].lower(),\
+                                               joints=['back','duct_tape','hip','knee','ankle'],\
+                                               device=configs[i],force_or_moment='moment')
+            np.savetxt('./Data/{}/{}_{}_reaction_moments.csv'.format(cases[i],config_names[i],load_type), out, fmt='%s', delimiter=',')
